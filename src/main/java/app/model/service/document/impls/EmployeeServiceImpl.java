864dc5363e477c979d9c854377593fcb433114dc
Sergei <sergei.visotsky@gmail.com>
2018-07-28 08:53:16 +0300

    Document exmple and test db credentials added

--- src/main/java/app/model/service/document/impls/EmployeeServiceImpl.java
+++ src/main/java/app/model/service/document/impls/EmployeeServiceImpl.java
@@ -1,4 +1,21 @@
 package app.model.service.document.impls;
 
-public class EmployeeServiceImpl {
+import app.model.jpa.dao.repos.EmployeeDao;
+import app.model.jpa.entities.Employee;
+import app.model.service.document.repos.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    @Override
+    public void readEmployeeData() {
+        Employee employee = new Employee();
+
+        employeeDao.addRecord(employee);
+    }
 }
--- src/main/java/app/model/service/document/repos/EmployeeService.java
+++ src/main/java/app/model/service/document/repos/EmployeeService.java
@@ -1,4 +1,8 @@
 package app.model.service.document.repos;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface EmployeeService {
+    void readEmployeeData();
 }
